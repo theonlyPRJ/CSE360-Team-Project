34f568a51fff0ecc3d1f0eb919d567844c030aed
Sanidhya Tiwari <stiwar60@asu.edu>
2026-09-20 20:18:25 -0700

    resolved conflicts

--- TP1-FA26/src/entityClasses/User.java
+++ TP1-FA26/src/entityClasses/User.java
@@ -3,22 +3,20 @@ package entityClasses;
 /*******
  * <p> Title: User Class </p>
  * 
- * <p> Description: This User class represents a user entity in the system.  It contains the user's
- *  details such as userName, password, and roles being played. </p>
+ * <p> Description: This User class represents a user entity in the system. It contains the user's
+ * details such as userName, password, roles being played, and One-Time Password (OTP) state. </p>
  * 
  * <p> Copyright: Lynn Robert Carter © 2025 </p>
  * 
  * @author Lynn Robert Carter
  * 
- * 
  */ 
 
 public class User {
-	
-	
-	/*
-	 * These are the private attributes for this entity object
-	 */
+    
+    /*
+     * These are the private attributes for this entity object
+     */
     private String userName;
     private String password;
     private String firstName;
@@ -32,37 +30,34 @@ public class User {
     private String oneTimePassword = null;
     private boolean isOtpActive = false;
     
-    
     /*****
      * <p> Method: User() </p>
      * 
      * <p> Description: This default constructor is not used in this system. </p>
      */
     public User() {
-    	
+        
     }
 
-    
     /*****
-     * <p> Method: User(String userName, String password, boolean r1, boolean r2,
-     * 		boolean r3, boolean r4, boolean r5) </p>
+     * <p> Method: User(String userName, String password, String fn, String mn, String ln, String pfn,
+     *      String ea, boolean r1, boolean r2, boolean r3) </p>
      * 
      * <p> Description: This constructor is used to establish user entity objects. </p>
      * 
      * @param userName specifies the account userName for this user
-     * 
      * @param password specifies the account password for this user
-     * 
-     * @param r1 specifies the the Admin attribute (TRUE or FALSE) for this user
-     * 
-     * @param r2 specifies the the Student attribute (TRUE or FALSE) for this user
-     * 
-     * @param r3 specifies the the Reviewer attribute (TRUE or FALSE) for this user
-     * 
+     * @param fn specifies the first name
+     * @param mn specifies the middle name
+     * @param ln specifies the last name
+     * @param pfn specifies the preferred first name
+     * @param ea specifies the email address
+     * @param r1 specifies the Admin attribute (TRUE or FALSE) for this user
+     * @param r2 specifies the Role1 / Contributor attribute (TRUE or FALSE) for this user
+     * @param r3 specifies the Role2 / Viewer attribute (TRUE or FALSE) for this user
      */
-    // Constructor to initialize a new User object with userName, password, and role.
     public User(String userName, String password, String fn, String mn, String ln, String pfn, 
-    		String ea, boolean r1, boolean r2, boolean r3) {
+            String ea, boolean r1, boolean r2, boolean r3) {
         this.userName = userName;
         this.password = password;
         this.firstName = fn;
@@ -75,139 +70,72 @@ public class User {
         this.role2 = r3;
     }
 
-    
     /*****
      * <p> Method: void setAdminRole(boolean role) </p>
      * 
      * <p> Description: This setter defines the Admin role attribute. </p>
      * 
      * @param role is a boolean that specifies if this user in playing the Admin role.
-     * 
      */
-    // Sets the role of the Admin user.
     public void setAdminRole(boolean role) {
-    	this.adminRole=role;
+        this.adminRole = role;
     }
 
-    
     /*****
      * <p> Method: void setRole1User(boolean role) </p>
      * 
      * <p> Description: This setter defines the role1 attribute. </p>
      * 
      * @param role is a boolean that specifies if this user in playing role1.
-     * 
      */
-    // Sets the role1 user.
     public void setRole1User(boolean role) {
-    	this.role1=role;
+        this.role1 = role;
     }
 
-    
+    // Setter for Contributor role alias
+    public void setContributorRole(boolean role) {
+        this.role1 = role;
+    }
+
     /*****
      * <p> Method: void setRole2User(boolean role) </p>
      * 
      * <p> Description: This setter defines the role2 attribute. </p>
      * 
      * @param role is a boolean that specifies if this user in playing role2.
-     * 
      */
-    // Sets the role2 user.
-    
     public void setRole2User(boolean role) {
-    	this.role2=role;
+        this.role2 = role;
     }
-    
+
+    // Setter for Viewer role alias
+    public void setViewerRole(boolean role) {
+        this.role2 = role;
+    }
+
+    // One-Time Password (OTP) Setters and Controls
     public void setOneTimePassword(String otp) { 
         this.oneTimePassword = otp; 
         this.isOtpActive = true; 
     }
 
-    
+    public void clearOtp() { 
+        this.oneTimePassword = null; 
+        this.isOtpActive = false; 
+    }
+
     /*****
-     * <p> Method: String getUserName() </p>
-     * 
-     * <p> Description: This getter returns the UserName. </p>
-     * 
-     * @return a String of the UserName
-     * 
-     */
-    // Gets the current value of the UserName.
+     * Standard Field Getters and Setters
+     *****/
     public String getUserName() { return userName; }
-
-    
-    /*****
-     * <p> Method: String getPassword() </p>
-     * 
-     * <p> Description: This getter returns the Password. </p>
-     * 
-     * @return a String of the password
-	 *
-     */
-    // Gets the current value of the Password.
     public String getPassword() { return password; }
-
-    
-    /*****
-     * <p> Method: String getFirstName() </p>
-     * 
-     * <p> Description: This getter returns the FirstName. </p>
-     * 
-     * @return a String of the FirstName
-	 *
-     */
-    // Gets the current value of the FirstName.
     public String getFirstName() { return firstName; }
-
-    
-    /*****
-     * <p> Method: String getMiddleName() </p>
-     * 
-     * <p> Description: This getter returns the MiddleName. </p>
-     * 
-     * @return a String of the MiddleName
-	 *
-     */
-    // Gets the current value of the Student role attribute.
     public String getMiddleName() { return middleName; }
-
-    
-    /*****
-     * <p> Method: String getLasteName() </p>
-     * 
-     * <p> Description: This getter returns the LastName. </p>
-     * 
-     * @return a String of the LastName
-	 *
-     */
-    // Gets the current value of the Student role attribute.
     public String getLastName() { return lastName; }
-
-    
-    /*****
-     * <p> Method: String getPreferredFirstName() </p>
-     * 
-     * <p> Description: This getter returns the PreferredFirstName. </p>
-     * 
-     * @return a String of the PreferredFirstName
-	 *
-     */
-    // Gets the current value of the Student role attribute.
     public String getPreferredFirstName() { return preferredFirstName; }
-
-    
-    /*****
-     * <p> Method: String getEmailAddress() </p>
-     * 
-     * <p> Description: This getter returns the EmailAddress. </p>
-     * 
-     * @return a String of the EmailAddress
-	 *
-     */
-    // Gets the current value of the Student role attribute.
     public String getEmailAddress() { return emailAddress; }
-    
     public String getOneTimePassword() { return oneTimePassword; }
+    public boolean isOtpActive() { return isOtpActive; }
 
     public void setUserName(String s) { userName = s; }
     public void setPassword(String s) { password = s; }
@@ -217,63 +145,20 @@ public class User {
     public void setPreferredFirstName(String s) { preferredFirstName = s; }
     public void setEmailAddress(String s) { emailAddress = s; }
 
-    
     /*****
-     * <p> Method: String getAdminRole() </p>
-     * 
-     * <p> Description: This getter returns the value of the Admin role attribute. </p>
-     * 
-     * @return a String of "TRUE" or "FALSE" based on state of the attribute
-	 *
-     */
-    // Gets the current value of the Admin role attribute.
+     * Role Getters & Helpers
+     *****/
     public boolean getAdminRole() { return adminRole; }
-
-    
-    /*****
-     * <p> Method: String getRole1() </p>
-     * 
-     * <p> Description: This getter returns the value of the role1 attribute. </p>
-     * 
-     * @return a String of "TRUE" or "FALSE" based on state of the attribute
-	 *
-     */
-    // Gets the current value of the role1 attribute.
-	public boolean getNewRole1() { return role1; }
-
-    
-    /*****
-     * <p> Method: String getRole2() </p>
-     * 
-     * <p> Description: This getter returns the value of the role2 attribute. </p>
-     * 
-     * @return a String of "TRUE" or "FALSE" based on state of the attribute
-	 *
-     */
-    // Gets the current value of the role2 attribute.
+    public boolean getNewRole1() { return role1; }
+    public boolean getContributorRole() { return role1; }
     public boolean getNewRole2() { return role2; }
+    public boolean getViewerRole() { return role2; }
 
-        
-    /*****
-     * <p> Method: int getNumRoles() </p>
-     * 
-     * <p> Description: This getter returns the number of roles this user plays (0 - 5). </p>
-     * 
-     * @return a value 0 - 5 of the number of roles this user plays
-	 *
-     */
-    // Gets the current value of the Staff role attribute.
     public int getNumRoles() {
-    	int numRoles = 0;
-    	if (adminRole) numRoles++;
-    	if (role1) numRoles++;
-    	if (role2) numRoles++;
-    	return numRoles;
-    }
-    
-    public boolean isOtpActive() { return isOtpActive; }
-    public void clearOtp() { 
-        this.oneTimePassword = null; 
-        this.isOtpActive = false; 
+        int numRoles = 0;
+        if (adminRole) numRoles++;
+        if (role1) numRoles++;
+        if (role2) numRoles++;
+        return numRoles;
     }
 }
